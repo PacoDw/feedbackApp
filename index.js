bdd7367c1023a596fe0afd03e3d286c4b70dd543
Paco Preciado Mendoza <__pm@outlook.com>
2018-08-22 08:29:51 -0500

    New title

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 3000;
 app
     .get('/', (req, res) => {
 
-        res.send('Hola mundo');
+        res.send('<h1>Hola mundo</h1>');
     })
     .listen(PORT, _ => console.log('Starting in port: 3000'));
